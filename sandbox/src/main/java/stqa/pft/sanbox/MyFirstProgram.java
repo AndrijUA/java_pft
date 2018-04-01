d3c2cde6837a153f35d11a928300612d3b2befa9
AndrijUA <andrzejza75@gmail.com>
2018-04-01 07:02:35 +0200

    Dodanie metod

--- sandbox/src/main/java/stqa/pft/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/stqa/pft/sanbox/MyFirstProgram.java
@@ -1,7 +1,28 @@
 package stqa.pft.sanbox;
 
-public class MyFirstProgram{
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-	}
+public class MyFirstProgram {
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Andrij");
+
+    double l = 5;
+    System.out.println("Kwadrat ze stroną " + l + " ma pole = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Prostokąt ze stronami " + a + " i " + b + " ma pole = " + area(a, b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
 }
