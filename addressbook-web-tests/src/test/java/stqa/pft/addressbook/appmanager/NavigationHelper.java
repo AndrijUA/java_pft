32abd55eb51233b2c5e76255c0b2372d3690deda
AndrijUA <andrzejza75@gmail.com>
2018-04-05 20:14:16 +0200

    Optymizacja przejść między stronami. Jeżeli jest otwarta strona, to przejście nie jest wykonywane.

--- addressbook-web-tests/src/test/java/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -6,17 +6,25 @@ import org.openqa.selenium.WebDriver;
 /**
  * Created by Iwona Zajac on 02.04.2018.
  */
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(WebDriver wd) {
     super(wd);
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
 
   public void gotoHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.linkText("home"));
   }
 }
