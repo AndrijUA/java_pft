05f31f57d782e7460a568367f3b73144a4332c26
AndrijUA <andrzejza75@gmail.com>
2018-04-06 21:58:11 +0200

    Porównanie w teście testGroupDeletion listy before i after bez elementu usuniętego

--- addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -14,7 +14,7 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    if (! app.getGroupHelper().isThereAGroup()){
+    if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
@@ -23,6 +23,9 @@ public class GroupDeletionTests extends TestBase {
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
 
 }
