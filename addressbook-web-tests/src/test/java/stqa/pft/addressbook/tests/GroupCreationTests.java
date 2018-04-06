e6e304c1b41ed00b3bf17f22ad92150736ee94f0
AndrijUA <andrzejza75@gmail.com>
2018-04-06 23:46:43 +0200

    Szukanie maksymalnej wartości id dla nowej grupy poprzez stream

--- addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -19,13 +20,15 @@ public class GroupCreationTests extends TestBase {
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+//    int max = 0;
+//    for (GroupData g : after) {
+//      if (g.getId() > max) {
+//        max = g.getId();
+//      }
+//    }
+
+//    group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
