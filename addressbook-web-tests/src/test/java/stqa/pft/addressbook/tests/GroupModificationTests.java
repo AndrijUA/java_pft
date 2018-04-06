67b290a28eadb6e76e2e0efc476b402e7e90da8f
AndrijUA <andrzejza75@gmail.com>
2018-04-06 22:30:37 +0200

    Porównanie listy grup przed modyfikacją i po modyfikacji w tescie testGroupModification

--- addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -12,18 +13,24 @@ import java.util.List;
 public class GroupModificationTests extends TestBase {
 
   @Test
-  public void testGroupModification(){
+  public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
-    if (! app.getGroupHelper().isThereAGroup()){
+    if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test4", "test5"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", "test4", "test5");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    //porównanie dwóch posortowanycj list
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
