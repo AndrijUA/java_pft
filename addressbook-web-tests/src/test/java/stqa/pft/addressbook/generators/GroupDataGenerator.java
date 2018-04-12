e0019b72aa89a4b48cea25eeae239ba034e65bed
AndrijUA <andrzejza75@gmail.com>
2018-04-12 20:59:16 +0200

    Dodano obsługę tworzenia danych i pobranie danych z pliku json

--- addressbook-web-tests/src/test/java/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,8 @@ package stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import stqa.pft.addressbook.model.GroupData;
 
@@ -45,8 +47,23 @@ public class GroupDataGenerator {
       saveAsCsv(groups, new File(file));
     } else if (format.equals("xml")) {
       saveAsXml(groups, new File(file));
+    } else if (format.equals("json")) {
+      saveAsJson(groups, new File(file));
     } else {
       System.out.println("Unrecognized format " + format);
+    }
+  }
+
+  //https://github.com/google/gson
+  //https://github.com/google/gson/blob/master/UserGuide.md
+  private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    String json = gson.toJson(groups);
+//    Writer writer = new FileWriter(file);
+//    writer.write(json);
+//    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(json);
     }
   }
 
@@ -56,17 +73,25 @@ public class GroupDataGenerator {
 //    xstream.alias("group", GroupData.class);
     xstream.processAnnotations(GroupData.class);
     String xml = xstream.toXML(groups);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+//    Writer writer = new FileWriter(file);
+//    writer.write(xml);
+//    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(xml);
+    }
   }
 
   private static void saveAsCsv(List<GroupData> groups, File file) throws IOException {
-    Writer writer = new FileWriter(file);
-    for (GroupData group : groups) {
-      writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+//    Writer writer = new FileWriter(file);
+//    for (GroupData group : groups) {
+//      writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+//    }
+//    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      for (GroupData group : groups) {
+        writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+      }
     }
-    writer.close();
   }
 
   private static List<GroupData> generateGroups(int count) {
--- addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,8 @@
 package stqa.pft.addressbook.tests;
 
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.Assert;
 import org.testng.annotations.*;
@@ -8,9 +10,7 @@ import stqa.pft.addressbook.model.GroupData;
 import stqa.pft.addressbook.model.Groups;
 
 import java.io.*;
-import java.nio.Buffer;
 import java.util.*;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -19,11 +19,11 @@ import static org.hamcrest.MatcherAssert.*;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroupsCsv() throws IOException {
+  public Iterator<Object[]> validGroupsWithCsv() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
     BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
     String line = reader.readLine();
-    while (line != null){
+    while (line != null) {
       String[] split = line.split(";");
       list.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
       line = reader.readLine();
@@ -32,30 +32,44 @@ public class GroupCreationTests extends TestBase {
   }
 
   @DataProvider
-  public Iterator<Object[]> validGroupsXml() throws IOException {
+  public Iterator<Object[]> validGroupsWithXml() throws IOException {
     BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
     String xml = "";
     String line = reader.readLine();
-    while (line != null){
+    while (line != null) {
       xml += line;
       line = reader.readLine();
     }
     XStream xStream = new XStream();
     xStream.processAnnotations(GroupData.class);
     List<GroupData> groups = (List<GroupData>) xStream.fromXML(xml);
-    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
   }
 
-  @Test(dataProvider = "validGroupsXml")
+  @DataProvider
+  public Iterator<Object[]> validGroupsWithJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {}.getType());
+    return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validGroupsWithJson")
   public void testGroupCreationProvider(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
     app.group().create(group);
-    assertThat(app.group().count(),equalTo(before.size()+1));
+    assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().all();
 
     before.add(group);
-    assertThat(after,equalTo(
+    assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
@@ -65,11 +79,11 @@ public class GroupCreationTests extends TestBase {
     Groups before = app.group().all();
     GroupData group = new GroupData().withName("test1").withHeader("test2").withFooter("test3");
     app.group().create(group);
-    assertThat(app.group().count(),equalTo(before.size()+1));
+    assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().all();
 
     before.add(group);
-    assertThat(after,equalTo(
+    assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
@@ -79,11 +93,11 @@ public class GroupCreationTests extends TestBase {
     Groups before = app.group().all();
     GroupData group = new GroupData().withName("test1'");
     app.group().create(group);
-    assertThat(app.group().count(),equalTo(before.size()));
+    assertThat(app.group().count(), equalTo(before.size()));
     Groups after = app.group().all();
 
     before.add(group);
-    assertThat(after,equalTo(before));
+    assertThat(after, equalTo(before));
   }
 
   @Test
@@ -126,11 +140,11 @@ public class GroupCreationTests extends TestBase {
     app.group().create(group);
     Set<GroupData> after = app.group().setAll();
 //    Assert.assertEquals(after.size(), before.size() + 1);
-    assertThat(after.size(),equalTo(before.size()+1));
+    assertThat(after.size(), equalTo(before.size() + 1));
 
     group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
     before.add(group);
 //    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
-    assertThat(after,equalTo(before));
+    assertThat(after, equalTo(before));
   }
 }
