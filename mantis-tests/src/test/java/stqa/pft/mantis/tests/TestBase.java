77b4aa77479a8fa24f165510cadf3ebb93840801
AndrijUA <andrzejza75@gmail.com>
2018-04-16 20:11:04 +0200

    Dodano nowy modul dla testów Mantis

---
+++ mantis-tests/src/test/java/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,25 @@
+package stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import stqa.pft.mantis.appmanager.ApplicationManager;
+
+/**
+ * Created by Iwona Zajac on 02.04.2018.
+ */
+public class TestBase {
+
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+  @BeforeSuite(alwaysRun = true)
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() throws Exception {
+    app.stop();
+  }
+}
